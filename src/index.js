fc4be6307256cf9df2ec9f9d6ddd5b916f769fd3
Gillpreet <gillpreetsinghgill@gmail.com>
2021-02-05 13:09:05 -0800

    adding header

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { GlobalStyles } from './global-styles';
+import App from './App';
+import { FirebaseContext } from './context/firebase';
+
+
+const config = {
+    apiKey: "AIzaSyCBfPUggYfts1IH9jVJLYi6_zU3dtFNuqY",
+    authDomain: "netflix-a5ceb.firebaseapp.com",
+    databaseURL: "https://netflix-a5ceb.firebaseio.com",
+    projectId: "netflix-a5ceb",
+    storageBucket: "netflix-a5ceb.appspot.com",
+    messagingSenderId: "420396607824",
+    appId: "1:420396607824:web:9920406932de9ea2c7e455"
+}
+
+const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
-  <>
-    <GlobalStyles />
-    <App />
-  </>,
-  document.getElementById('root')
+    <>
+        <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+            <GlobalStyles />
+            <App />
+        </FirebaseContext.Provider>
+    </>,
+    document.getElementById('root')
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import { FaqsContainer } from '../container/faqs'
+import { Feature, OptForm } from '../components'
 import { JumbotronContainer } from '../container/jumbotron'
 import { FooterContainer } from '../container/Footer'
+import { HeaderContainer } from '../container/Header' 
 
 export default function Home() {
     return (
         <>
+            <HeaderContainer>
+                <Feature>
+                    <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+                    <Feature.Subtitle>Watch any anywhere. Cancel at any time.</Feature.Subtitle>
+                    <OptForm>
+                        <OptForm.Input placeholder='Email Address' />
+                        <OptForm.Button>Try it now</OptForm.Button>
+                        <OptForm.Break></OptForm.Break>
+                        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+                    </OptForm>
+                </Feature>
+            </HeaderContainer>
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
