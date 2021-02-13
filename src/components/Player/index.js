b3c8ae25c02d9c32c858d6097a2f24b5b7030e07
Gillpreet <gillpreetsinghgill@gmail.com>
2021-02-12 18:58:07 -0800

    done Netflix project

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -4,7 +4,6 @@ import {
     Container,
     Overlay,
     Inner, 
-    Close,
     Button
 } from './styles/Player'
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-//import 'normalize.css';
 import { GlobalStyles } from './global-styles';
 import  App  from './App';
 import { firebase } from './lib/firebase.prod';
